7623addda2816877ffd51aef0515721d82ebe5f8
excf28 <deee4444@gmail.com>
2023-04-08 20:20:48 +0200

    initial commit

---
+++ src/cm-chessboard/model/ChessboardState.js
@@ -0,0 +1,37 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/cm-chessboard
+ * License: MIT, see file 'LICENSE'
+ */
+import {Position} from "./Position.js"
+
+export class ChessboardState {
+
+    constructor() {
+        this.position = new Position()
+        this.orientation = undefined
+        this.markers = []
+        this.inputWhiteEnabled = false
+        this.inputBlackEnabled = false
+        this.inputEnabled = false
+        this.squareSelectEnabled = false
+        this.extensionPoints = {}
+        this.moveInputProcess = Promise.resolve()
+    }
+
+    invokeExtensionPoints(name, data = {}) {
+        const extensionPoints = this.extensionPoints[name]
+        const dataCloned = Object.assign({}, data)
+        dataCloned.extensionPoint = name
+        let returnValue = true
+        if (extensionPoints) {
+            for (const extensionPoint of extensionPoints) {
+                if(extensionPoint(dataCloned) === false) {
+                    returnValue = false
+                }
+            }
+        }
+        return returnValue
+    }
+
+}
