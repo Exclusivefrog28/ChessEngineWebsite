72eecb5e0b5b8e6b9041fc6c6ae766410a4c5e67
excf28 <deee4444@gmail.com>
2023-04-28 17:49:36 +0200

    promotion ui

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import {INPUT_EVENT_TYPE, Chessboard} from "../src/cm-chessboard/Chessboard.js"
 import {FEN} from "../src/cm-chessboard/model/Position.js"
 import {MARKER_TYPE, Markers} from "../src/cm-chessboard/extensions/markers/Markers.js"
+import {PromotionDialog} from "../src/cm-chessboard/extensions/promotion-dialog/PromotionDialog.js"
+import {COLOR, PIECE} from "./src/cm-chessboard/Chessboard.js";
 
 function getSquareIndex(square) {
     const file = square.charCodeAt(0) - 97;
     const rank = 8 - parseInt(square.charAt(1));
     return (rank * 8) + file;
 }
+
 function getSquare(index) {
     const file = String.fromCharCode(97 + index % 8);
     const rank = 8 - Math.floor(index / 8);
@@ -17,37 +20,35 @@ window.board = new Chessboard(document.getElementById("board"), {
     position: FEN.start,
     assetsUrl: "../assets/",
     style: {pieces: {file: "staunty.svg"}},
-    extensions: [{class: Markers}]
+    extensions: [{class: PromotionDialog}, {class: Markers}]
 })
 
-function showAttacks(){
-    window.board.removeMarkers(MARKER_TYPE.circleDanger)
-    let attacks = Module.ccall('getAttacks', 'string').split(" ")
-    for (let i = 0; i < attacks.length; i++) {
-        window.board.addMarker(MARKER_TYPE.circleDanger, attacks[i])
-    }
+function evaluate() {
+    let evaluation = Module.ccall('eval', 'int')
+    console.log(evaluation)
+
 }
 
 Module.ccall("init")
-showAttacks()
 window.board.enableMoveInput(inputHandler)
 
 document.getElementById("unmake").addEventListener("click", function () {
     let fen = Module.ccall("unmove", 'string')
-    window.board.setPosition(fen, false)
-    showAttacks()
+    window.board.setPosition(fen, true)
+    evaluate()
 })
 
 document.getElementById("perftButton").addEventListener("click", function () {
     let start = new Date().getTime();
     let depth = document.getElementById("depth").value
     let fen = document.getElementById("fen").value
-    let nodes = Module.ccall("runPerft", 'number', ['number','string'], [depth,fen])
+    let nodes = Module.ccall("runPerft", 'number', ['number', 'string'], [depth, fen])
     let end = new Date().getTime();
     log(`perft ${depth}: ${nodes} time: ${end - start}ms`)
 })
 
 let moves;
+
 function inputHandler(event) {
     window.board.removeMarkers(MARKER_TYPE.frame)
     window.board.removeMarkers(MARKER_TYPE.dot)
@@ -62,15 +63,43 @@ function inputHandler(event) {
         case INPUT_EVENT_TYPE.validateMoveInput:
             let move
             for (let i = 0; i < moves.length; i++) {
-                if(moves[i].end === event.squareTo){
+                if (moves[i].end === event.squareTo) {
                     move = moves[i]
                     break
                 }
             }
-            let fen = Module.ccall('move', 'string', ['number', 'number', 'number', 'number', 'number'], [getSquareIndex(move.start), getSquareIndex(move.end), move.flag, move.promotionType, move.player])
-            window.board.setPosition(fen, false)
-            showAttacks()
-            break
+
+            if (move.promotionType != 0) {
+                window.board.showPromotionDialog(event.squareTo, (move.player == 0) ? COLOR.white : COLOR.black, (result) => {
+                    switch (result.piece) {
+                        case PIECE.wn:
+                        case PIECE.bn:
+                            move.promotionType = 2
+                            break
+                        case PIECE.wb:
+                        case PIECE.bb:
+                            move.promotionType = 3
+                            break
+                        case PIECE.wr:
+                        case PIECE.br:
+                            move.promotionType = 4
+                            break
+                        case PIECE.wq:
+                        case PIECE.bq:
+                            move.promotionType = 5
+                            break
+                    }
+                    let fen = Module.ccall('move', 'string', ['number', 'number', 'number', 'number', 'number'], [getSquareIndex(move.start), getSquareIndex(move.end), move.flag, move.promotionType, move.player])
+                    window.board.setPosition(fen, true)
+                    evaluate()
+                })
+            } else{
+                let fen = Module.ccall('move', 'string', ['number', 'number', 'number', 'number', 'number'], [getSquareIndex(move.start), getSquareIndex(move.end), move.flag, move.promotionType, move.player])
+                window.board.setPosition(fen, true)
+                evaluate()
+                break
+            }
+            return true
         case INPUT_EVENT_TYPE.moveInputCanceled:
             break
     }
